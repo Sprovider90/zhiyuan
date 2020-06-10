2e02b46eef464762b780d0fd6404467d9fd64041
vagrant <vagrant@homestead>
2020-06-10 05:35:57 +0000

    apibash

--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::prefix('v1')->name('api.v1.')->group(function() {
-    Route::get('version', function() {
-        return 'this is version v1';
-    })->name('version');
-});
 
-Route::prefix('v2')->name('api.v2.')->group(function() {
-    Route::get('version', function() {
-        return 'this is version v2';
-    })->name('version');
 });
