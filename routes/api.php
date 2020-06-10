c09ae4be916cebf58148329d79bdbde1dbf31757
vagrant <vagrant@homestead>
2020-06-10 09:12:15 +0000

    使用 JWT

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,10 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
         // 登录
     Route::post('authorizations', 'AuthorizationsController@store')
         ->name('api.authorizations.store');
+         // 刷新token
+    Route::put('authorizations/current', 'AuthorizationsController@update')
+        ->name('authorizations.update');
+    // 删除token
+    Route::delete('authorizations/current', 'AuthorizationsController@destroy')
+        ->name('authorizations.destroy');
 });
