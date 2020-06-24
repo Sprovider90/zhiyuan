f1c221eef0f5d4d72b440efd8d44b5afe4794649
vagrant <vagrant@homestead>
2020-06-24 10:16:06 +0000

    dictory

---
+++ database/migrations/2020_06_24_174714_seed_dictory_data.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Dictory;
+
+class SeedDictoryData extends Migration
+{
+    public function up()
+    {
+        // 清除缓存
+        app()['cache']->forget('spatie.permission.cache');
+        $arr=[1=>"值1",2=>"值2",3=>"值3"];
+        $value_jsons=json_encode($arr);
+        // 先创建权限
+        Dictory::create(['name' => 'xailai1',"value"=>$value_jsons]);
+        
+    }
+
+    public function down()
+    {
+        // 清除缓存
+        app()['cache']->forget('spatie.permission.cache');
+
+        Model::unguard();
+        DB::table($tableNames['dictory'])->delete();
+        Model::reguard();
+    }
+}
