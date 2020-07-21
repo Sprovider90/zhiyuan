4afccebd1cbc6faa5f00fca53248aee5de20eafb
Sprovider90 <vagrant@homestead>
2020-07-21 10:15:03 +0000

    create table

---
+++ database/migrations/2020_07_21_142731_create_message_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('type')->default(1)->comment('消息类型 1系统消息 2预警消息');
+            $table->string('content')->comment('消息内容');
+            $table->text('rev_users')->comment('接收的账号id');
+            $table->timestampTz('send_time',0)->nullable()->comment('消息触发的时候时间');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
