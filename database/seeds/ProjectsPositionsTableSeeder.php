170a1da7298d3619bc5310da2c28bba6c911e768
Justin <763114070@qq.com>
2020-07-30 17:05:26 +0800

    up

--- database/seeds/ProjectsPositionsTableSeeder.php
+++ database/seeds/ProjectsPositionsTableSeeder.php
@@ -1,6 +1,11 @@
 <?php
+
+use App\Models\Projects;
+use Faker\Generator;
 use Illuminate\Database\Seeder;
 use App\Models\ProjectsPositions;
+use App\Models\ProjectsAreas;
+
 class ProjectsPositionsTableSeeder extends Seeder
 {
     /**
@@ -10,9 +15,19 @@ class ProjectsPositionsTableSeeder extends Seeder
      */
     public function run()
     {
+        $project_ids = Projects::all()->pluck('id')->toArray();
+        $project_area_ids = ProjectsAreas::all()->pluck('id')->toArray();
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
         $projectsPositions = factory(ProjectsPositions::class)
-            ->times(10)
-            ->make();
+            ->times(1000)
+            ->make()
+            ->each(function ($projectPos, $index)
+            use ($project_ids,$project_area_ids, $faker)
+            {
+                $projectPos->project_id = $faker->randomElement($project_ids);
+                $projectPos->area_id = $faker->unique()->randomElement($project_area_ids);
+            });
 
 
         // 将数据集合转换为数组，并插入到数据库中
