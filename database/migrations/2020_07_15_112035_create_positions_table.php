b0c4fc00d2d8229c733e4986177daaf866b31655
Justin <763114070@qq.com>
2020-08-03 14:02:00 +0800

    up

--- database/migrations/2020_07_15_112035_create_positions_table.php
+++ database/migrations/2020_07_15_112035_create_positions_table.php
@@ -18,9 +18,9 @@ class CreatePositionsTable extends Migration
             $table->integer('project_id')->comment('项目ID');
             $table->string('number')->comment('点位编号');
             $table->string('name')->comment('点位名称');
-            $table->string('good_id')->default(1)->comment('产品id  默认1  1空气净化器');
-            $table->string('device_id')->nullable()->comment('设备id');
-            $table->string('area_id')->comment('该项目下的区域ID');
+            $table->integer('good_id')->default(1)->comment('产品id  默认1  1空气净化器');
+            $table->integer('device_id')->nullable()->comment('设备id');
+            $table->integer('area_id')->comment('该项目下的区域ID');
             $table->tinyInteger('status')->default(1)->comment('状态 1启用 2停用 默认1');
             $table->softDeletes();
             $table->timestamps();
