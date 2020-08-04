c3ec3471d7c44b15703078953fc596353de6cd6e
Justin <763114070@qq.com>
2020-08-04 11:32:52 +0800

    up

--- database/seeds/OrdersDevicesTableSeeder.php
+++ database/seeds/OrdersDevicesTableSeeder.php
@@ -15,14 +15,14 @@ class OrdersDevicesTableSeeder extends Seeder
         // 获取 Faker 实例
         $faker = app(Faker\Generator::class);
         $ordersDevices = factory(\App\Models\OrdersDevices::class)
-            ->times(50)
+            ->times(300)
             ->make()
             ->each(function ($data, $index) use ($order_ids, $faker)
             {
                 $order_id = $faker->randomElement($order_ids);
                 $data->order_id = $order_id;
                 $order = \App\Models\Orders::find($order_id);
-                $data->customer_id = $order->cid;
+//                $data->customer_id = $order->cid;
             });
         // 将数据集合转换为数组，并插入到数据库中
         \App\Models\OrdersDevices::insert($ordersDevices->toArray());
