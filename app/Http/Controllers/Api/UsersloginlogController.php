d7319cd3afe5bcf97045443b00ab7ef10c7355cc
vagrant <vagrant@homestead>
2020-08-20 12:03:16 +0000

    dicdebug

--- app/Http/Controllers/Api/UsersloginlogController.php
+++ app/Http/Controllers/Api/UsersloginlogController.php
@@ -13,6 +13,13 @@ class UsersloginlogController extends Controller
 {
 	public function index(Request $request,Loginlog $Loginlog)
     {
+//        if(isset($request->reuse_param)&&!empty($request->reuse_param)){
+//            $query =$user->where('name','like','%'.$request['reuse_param'].'%')
+//                ->orWhere('truename','like','%'.$request['reuse_param'].'%')
+//                ->orWhere('phone','like','%'.$request['reuse_param'].'%')
+//                ->orWhere('id',$request['reuse_param']);
+//        }
+
         $Loginlog = QueryBuilder::for(Loginlog::class)
             ->allowedIncludes('users')
             ->paginate($request->pageSize ?? $request->pageSize);
