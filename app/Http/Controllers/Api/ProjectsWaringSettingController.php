0525cd2d03e8e17926fe42863d2ad57dd8ce342e
Sprovider90 <vagrant@homestead>
2020-08-21 09:57:33 +0000

    项目预警设置

---
+++ app/Http/Controllers/Api/ProjectsWaringSettingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\ProjectsWaringSettingRequest;
+use App\Http\Resources\ProjectsWaringSettingResource;
+use App\Models\Projects;
+use App\Models\ProjectsWaringSetting;
+use Illuminate\Http\Request;
+
+class ProjectsWaringSettingController extends Controller
+{
+    public function store(ProjectsWaringSettingRequest $request, ProjectsWaringSetting $waringsetting,Projects $project)
+    {
+        $waringsetting->fill($request->all());
+        $waringsetting->project_id=$project->id;
+        $waringsetting->save();
+
+        return new ProjectsWaringSettingResource($waringsetting);
+    }
+
+    public function update(Projects $project,ProjectsWaringSetting $waringsetting,ProjectsWaringSettingRequest $request)
+    {
+    
+        $waringsetting->update($request->all());
+        return new ProjectsWaringSettingResource($waringsetting);
+    }
+}
