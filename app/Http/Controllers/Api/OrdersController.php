7080a2bc8a6f308486ae7d6aa2b26513a5997530
Justin <763114070@qq.com>
2020-09-02 14:13:32 +0800

    up

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -53,7 +53,7 @@ class OrdersController extends Controller
         //待付订单总金额
         $order_no_pay_money_count = 0;
         $order_no_pay_money = $orders->whereIn('order_status',[1,3])->whereBetween('created_at',$date)->get();;
-        if($order_no_pay_money_count){
+        if($order_no_pay_money){
             foreach ($order_no_pay_money as $k => $v){
                 $order_no_pay_money_count = $order_no_pay_money_count + $v->moeny;
                 if($v->status == 3){
