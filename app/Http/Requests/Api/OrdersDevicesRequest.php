eded7d624501a1d54ed68a702800bbc2512a1158
Justin <763114070@qq.com>
2020-09-03 13:20:10 +0800

    u

--- app/Http/Requests/Api/OrdersDevicesRequest.php
+++ app/Http/Requests/Api/OrdersDevicesRequest.php
@@ -34,15 +34,15 @@ class OrdersDevicesRequest extends FormRequest
                     if(!is_array($data)){
                         return $fail($attribute.' is invalid.');
                     }
-                    $arr = ['number'];
+                    $arr = ['device_id'];
                     foreach ($data as $k => $v){
                         foreach ($arr as $k1 => $v1){
                             if(!isset($v[$v1]) || empty($v[$v1])){
                                 return $fail('array['.$k.'] '.$v1.' is empty.');
                                 break;
                             }
-                            if($v1 == 'number'){
-                                $flg = Device::where('device_number',$v[$v1])->where('status',1)->whereNull('customer_id')->whereNull('deleted_at')->first();
+                            if($v1 == 'device_id'){
+                                $flg = Device::where('id',$v[$v1])->where('status',1)->whereNull('customer_id')->first();
                                 if(!$flg){
                                     return $fail('array['.$k.'] '.$v1.' 设备不存在');
                                     break;
