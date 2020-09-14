f91b32add006cb9dbb5fee66a483a4d696fbc176
Justin <763114070@qq.com>
2020-09-14 11:47:53 +0800

    'up'

--- app/Http/Controllers/Api/PublicController.php
+++ app/Http/Controllers/Api/PublicController.php
@@ -211,6 +211,7 @@ class PublicController extends Controller
             }
         }
         //预警警报
+        $Warnigs = Warnigs::query();
         if($request->user()->customer_id){
             $projects=Projects::where("customer_id",$request->user()->customer_id)->pluck("id")->toArray();
             if(!empty($projects)){
