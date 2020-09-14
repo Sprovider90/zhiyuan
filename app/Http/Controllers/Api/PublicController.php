f348707b6b1870e1d98d266040cc6b9734ceaa4f
Justin <763114070@qq.com>
2020-09-14 22:32:19 +0800

    'u''

--- app/Http/Controllers/Api/PublicController.php
+++ app/Http/Controllers/Api/PublicController.php
@@ -69,8 +69,9 @@ class PublicController extends Controller
         if($data){
             $res['project']['threshold_name'] = $data->thresholds_name ?? '';
             foreach ($data as $k => $v){
-                echo $res['data']['0'][$k].PHP_EOL;
                 $arr = explode('~',$data[$k]);
+                echo '当前值：'.$res['data']['0'][$k].PHP_EOL;
+                echo '检测值：'.$arr[0].'~'.$arr[1].PHP_EOL;
                 switch ($res['data']['0'][$k]){
                     case $res['data']['0'][$k] < $arr[0]:
                         $res['data']['0'][$k.'_tag'] = 1;
