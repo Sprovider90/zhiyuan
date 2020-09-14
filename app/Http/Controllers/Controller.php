004a4a2a76169646ce2c4622f65091053c137f30
Justin <763114070@qq.com>
2020-09-14 18:00:35 +0800

    'u'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -46,7 +46,7 @@ class Controller extends BaseController
                 AND a.stage_id IS NOT NULL
                 AND c.thresholdinfo IS NOT NULL
                 AND a.id='.$project_id);
-            var_dump($data[0]);exit;
+            var_dump($data[0]['formaldehyde']);exit;
             return $data ? json_decode($data[0],true)['thresholdinfo'] : '';
     }
 
