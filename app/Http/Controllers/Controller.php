d20a2a8ee8eefc55dcd8e11a1d77b8e9519050f2
Justin <763114070@qq.com>
2020-09-14 18:15:30 +0800

    'u'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -46,7 +46,7 @@ class Controller extends BaseController
                 AND a.stage_id IS NOT NULL
                 AND c.thresholdinfo IS NOT NULL
                 AND a.id='.$project_id);
-            return $data ? json_decode($data[0],true) : '';
+            return $data ? $data[0] : '';
     }
 
     public function getChinaName($string){
