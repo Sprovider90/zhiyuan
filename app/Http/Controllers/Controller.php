c7a4e0c55d796668cab1b946b7c81d0e86f86919
Justin <763114070@qq.com>
2020-09-14 18:14:09 +0800

    'up'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -46,6 +46,7 @@ class Controller extends BaseController
                 AND a.stage_id IS NOT NULL
                 AND c.thresholdinfo IS NOT NULL
                 AND a.id='.$project_id);
+            $data = $data[0];
             return $data ? json_decode($data->thresholdinfo,true) : '';
     }
 
