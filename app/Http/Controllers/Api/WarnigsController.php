f28628e54ea8b7bb12cacafa330a53741aba0a5e
vagrant <vagrant@homestead>
2020-09-17 15:04:52 +0000

    1

--- app/Http/Controllers/Api/WarnigsController.php
+++ app/Http/Controllers/Api/WarnigsController.php
@@ -28,7 +28,7 @@ class WarnigsController extends Controller
             $Warnigs = $Warnigs->whereBetween('created_at',[$request->start_time,$request->end_time]);
         }
 
-        $request->threshold_keys      && $Warnigs = $Warnigs->where('threshold_keys','like',"%{$request->threshold_keys},%");
+        $request->threshold_keys      && $Warnigs = $Warnigs->where('threshold_keys','like',"%{$request->threshold_keys}%");
 
         $request->reuseparam      && $Warnigs = $Warnigs->whereHas('project',function($query) use ($request){
             $query->where('name','like',"%{$request->reuseparam}%");
