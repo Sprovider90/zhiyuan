fd9f3208861ce4bd8acf5804260ba6472295b356
Justin <763114070@qq.com>
2020-09-19 00:00:51 +0800

    'u'

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -110,7 +110,9 @@ class ProjectsController extends Controller
     public function show(Projects $project,ProjectsStages $projectsStages)
     {
 
-        $data = $project->load(['stages','stages.thresholds'])->load(['position','position.areas'])->load('customs')->load('areas','areas.file');
+        $data = $project->load(['stages' => function($query){
+            $query->orderBy('stages','asc')->orderBy('id','desc');
+        },'stages.thresholds'])->load(['position','position.areas'])->load('customs')->load('areas','areas.file');
         $date = $projectsStages->where('project_id',$project->id);
         $data['start_time'] = $date->min('start_date');
         $data['end_time']   = $date->max('end_date');
