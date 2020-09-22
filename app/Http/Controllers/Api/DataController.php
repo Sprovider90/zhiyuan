9d710b13eff7449ad7e56fb27e529a7aa75fdc86
Justin <763114070@qq.com>
2020-09-22 10:12:05 +0800

    up

--- app/Http/Controllers/Api/DataController.php
+++ app/Http/Controllers/Api/DataController.php
@@ -24,7 +24,9 @@ class DataController extends Controller
                           $query->where('name','like',"%{$request->keyword}%");
                       });
             });
-        $request->soc && $position->device->where('soc','<=',$request->soc);
+        $request->soc && $position->whereHas('device' , function($query) use ($request) {
+            $query->where('soc','<',$request->soc);
+        });
         $position = $position->orderBy('id','desc')->paginate($request->pageSize ?? $request->pageSize);
         foreach ($position as $k => $v){
             $tag = Tag::where('model_type',3)->where('model_id',$v->id)->orderBy('id','desc')->first();
