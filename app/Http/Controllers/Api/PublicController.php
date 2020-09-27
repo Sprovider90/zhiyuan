d848732e3fa97680833dc19baa7cc81fd902695a
Justin <763114070@qq.com>
2020-09-27 22:10:47 +0800

    'u'

--- app/Http/Controllers/Api/PublicController.php
+++ app/Http/Controllers/Api/PublicController.php
@@ -137,23 +137,21 @@ class PublicController extends Controller
                 $dateList[$k]['count'] = 0;
             }
         }else {
-            if($request->type == 4){
-                $start_date = date("Y-m-d",strtotime(Projects::max('created_at'))) ;
-                $end_date = date("Y-m-d",strtotime(Projects::min('created_at')));
+
+            if($request->type == 4 || !$request->type){
+                $start_date = date("Y-m-d",strtotime(Projects::min('created_at'))) ;
+                $end_date = date("Y-m-d",strtotime(Projects::max('created_at')));
             }else{
                 $pro_date = $this->returnDate($request->type ?? 1);
                 $start_date = substr($pro_date[0], 0, 10);
                 $end_date = substr($pro_date[1], 0, 10);
-                if($request->start_date && $request->end_date){
-                    $pro_date = $this->returnDateList($request->start_date,$request->end_date);
-                    $start_date = $request->start_date;
-                    $end_date = $request->end_date;
-                }
             }
-            var_dump($start_date);
-            var_dump($end_date);
+            if($request->start_date && $request->end_date){
+                $pro_date = $this->returnDateList($request->start_date,$request->end_date);
+                $start_date = $request->start_date;
+                $end_date = $request->end_date;
+            }
             $dateList = $this->returnDateList($start_date, $end_date);
-            var_dump($dateList);exit;
             $proDateList = DB::select('select count(id) as num ,left(created_at,10) as date FROM projects where left(created_at,10) between "'.$start_date.'" AND "'.$end_date.'"  GROUP BY date ORDER BY date');
             $date = array_column($proDateList,'date');
             $dateNum = array_column($proDateList,'num','date');
