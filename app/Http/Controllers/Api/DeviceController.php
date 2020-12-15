3d6b4217eb5431e0ada9c3b10b309198885363c8
liang jucai <763114070@qq.com>
2020-12-15 19:50:33 +0800

    'u'

--- app/Http/Controllers/Api/DeviceController.php
+++ app/Http/Controllers/Api/DeviceController.php
@@ -42,7 +42,9 @@ class DeviceController extends Controller
     }
 
     public function index(Request $request,Device $device){
-        $device = $device->with(['storehouse','customer','customer.user']);
+        $device = $device->with(['storehouse','customer','customer.user'=>function($query){
+            $query->select(['name','truename']);
+        }]);
         //增加客户登录
         $request->user()->customer_id && $device = $device->where('customer_id',$request->user()->customer_id);
         $request->device_number &&   $device = $device->where('device_number','like',"%{$request->device_number}%");
