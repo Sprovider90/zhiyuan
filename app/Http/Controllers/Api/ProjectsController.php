9e88d2769fff0231fd433cf21bd20532c4a267d7
Justin <763114070@qq.com>
2021-02-10 00:09:18 +0800

    'u'

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -70,12 +70,12 @@ class ProjectsController extends Controller
                 ->orWhere('number','like',"%{$request->name}%");
         });*/
         $request->name   && $projects_query=$projects->where(function($query) use ($request){
-            $query->OrWhereHas('customs',function($q1) use ($request) {
+            $query->whereHas('customs',function($q1) use ($request) {
                 $q1->orWhere('company_name', 'like', "%{$request->name}%")
                     ->orWhere('company_addr', 'like', '%' . $request->name . '%')
                     ->orWhere('name', 'like', "%{$request->name}%");
             });
-            $query->OrWhereHas('position.device',function($q2) use ($request){
+            $query->whereHas('position.device',function($q2) use ($request){
                     $q2->orWhere('number','like',"%{$request->name}%");
                 });
         });
