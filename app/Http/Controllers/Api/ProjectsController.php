1c496c042ed71836eedde5ed7dff1860faf2c91e
Justin <763114070@qq.com>
2021-02-10 00:59:34 +0800

    'u'

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -60,8 +60,8 @@ class ProjectsController extends Controller
      */
     public function index(Request $request , Projects $projects)
     {
-        DB::connection()->enableQueryLog();
-        $projects_query=Projects::with(['customs','position_to_many','position.device']);
+//        DB::connection()->enableQueryLog();
+        $projects_query=Projects::with(['customs','position.device']);
         isset($request->status) && $request->status !=='' && $projects_query=$projects->where('status',$request->status);
         /*$request->name   && $projects_query=$projects->whereHas('customs',function($query) use ($request){
             $query->where('company_name','like',"%{$request->name}%")
@@ -84,7 +84,7 @@ class ProjectsController extends Controller
             ->allowedIncludes('customs','thresholds','waringsetting')
             ->orderBy('id','desc')
             ->paginate($request->pageSize ?? $request->pageSize);
-        dump(DB::getQueryLog());
+//        dump(DB::getQueryLog());
        // $projects = $projects->orderBy('id','desc')->paginate($request->pageSize ?? $request->pageSize);
         foreach ($projects as $k => $v){
             $v->position_count = Position::where('project_id',$v->id)->count();
